bdbd2d663e5c140b8e9cf18288d29e719cf92854
Kadir GÖNEN <kadirgnen3@gmail.com>
2022-11-05 10:58:13 +0300

    Add to 1.sql knowledges
    Add to 1.sql knowledges

--- Works/1.sql
+++ Works/1.sql
@@ -4,3 +4,7 @@ SELECT title,description FROM book
 /* 2 */
 SELECT * FROM book
 WHERE length>40 AND length<55
+
+/* 3 */
+SELECT * FROM book
+WHERE (rental_rate=0.98) AND (replacement_cost=11.69 OR replacement_cost=27.99)
