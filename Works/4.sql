57708bf36c8c2ec945bfbffc9030c12cb99e6752
Kadir GÖNEN <kadirgnen3@gmail.com>
2023-01-31 14:33:22 +0300

    Add to 4.sql
    Add to 4.sql

--- Works/4.sql
+++ Works/4.sql
@@ -4,6 +4,10 @@ SELECT DISTINCT replacement_cost FROM book
 /* 2 */
 SELECT COUNT(DISTINCT replacement_cost) FROM book
 
--- 3
+/* 3 */
 SELECT COUNT(title) FROM book
 WHERE title LIKE 'T%' AND rating='G'
+
+/* 4 */
+SELECT COUNT(country) FROM country
+WHERE LENGTH(country)=5
